f620e18170847c808aec807dea258c977c971046
himanshusalal <himanshusalal517@gmail.com>
2026-05-24 13:02:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,33 +4,26 @@ function Person(name, age) {
   this.age = age;
 }
 
-// Person method
 Person.prototype.greet = function () {
   console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
 };
 
-// Employee Constructor
 function Employee(name, age, jobTitle) {
-  // Call Person constructor
   Person.call(this, name, age);
 
   this.jobTitle = jobTitle;
 }
 
-// Inherit Person prototype
 Employee.prototype = Object.create(Person.prototype);
 
-// Fix constructor
 Employee.prototype.constructor = Employee;
 
-// Employee method
 Employee.prototype.jobGreet = function () {
   console.log(
     `Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`
   );
 };
 
-// Demo Function
 function runDemo() {
   const person = new Person("Alice", 25);
   person.greet();
@@ -39,6 +32,5 @@ function runDemo() {
   employee.jobGreet();
 }
 
-// Do not change below
 window.Person = Person;
 window.Employee = Employee;
